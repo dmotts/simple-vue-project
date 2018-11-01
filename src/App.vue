4cb82d07589cbfe4773ee8fbe59c5d50204c3f2b
Daley <daley.mottley@hotmail.com>
2018-11-01 01:59:16 +0000

    name slots

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <div class="row">
       <div class="col-xs-12">
         <app-quote>
-            <h2>The Quote</h2>
-            <p>A wonderful quote</p>
+            <h2 slot="title">{{ quoteTitle }}</h2>
+            <p slot="content">A wonderful quote</p>
         </app-quote>    
       </div>
     </div>
@@ -15,12 +15,16 @@
 import Quote from './components/Quote.vue'
 
 export default {
+  data: function() {
+    return {
+      quoteTitle: 'The Quote'
+    }
+  },
   components: {
     appQuote: Quote
   }
 }
 </script>
 
-<style>
-
+<style scoped>
 </style>
