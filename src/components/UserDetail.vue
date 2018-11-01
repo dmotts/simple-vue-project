b161678f762dac7a9132766fcc6ed5bbb0a612ee
Daley <daley.mottley@hotmail.com>
2018-11-01 01:22:28 +0000

    introduce props and emits

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,10 +2,27 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
+        <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
 <script>
+    export default {
+        props: {
+            myName: {
+                type: String,            }
+        },
+        methods: {
+            switchName() {
+                return this.myName.split("").reverse().join("");
+            },
+            resetName() {
+                this.myName = 'Daley';
+                this.$emit('nameWasReset', this.myName);
+            }
+        }
+    }
 </script>
 
 <style scoped>
